2c36b9a88d85d77f942824145081b5b3a33603e8
M'bazie Kone <mbazie89@gmail.com>
2024-12-28 14:27:07 +0100

    Updates

--- src/main/resources/static/js/modalInputAutofocus.js
+++
@@ -1,5 +0,0 @@
-const exampleModal = document.getElementById('registerModal');
-  exampleModal.addEventListener('shown.bs.modal', function () {
-    const inputField = document.getElementById('reg_username');
-    inputField.focus();
- });
